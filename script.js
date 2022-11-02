9d88cd60cda9839987e21d5c16f845dbd20e2b7e
Costan Daniel Solomon <danielcostan99@gmail.com>
2022-11-02 16:24:14 +0200

    second commit

--- script.js
+++ script.js
@@ -14,7 +14,6 @@ console.log(document.querySelector('.guess').value);
 */
 const SecretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-document.querySelector(".number").textContent = SecretNumber;
 
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
@@ -25,8 +24,10 @@ document.querySelector(".check").addEventListener("click", function () {
     //When player wins
   } else if (guess === SecretNumber) {
     document.querySelector(".message").textContent = "Correct Number!";
+    document.querySelector(".number").textContent = SecretNumber;
 
     document.querySelector("body").style.backgroundColor = "#60b347";
+    document.querySelector(".number").style.width = "30rem";
   } else if (guess > SecretNumber) {
     if (score > 1) {
       document.querySelector(".message").textContent = "Too high!";
@@ -44,4 +45,15 @@ document.querySelector(".check").addEventListener("click", function () {
       document.querySelector(".message").textContent = "You lost the game!";
     }
   }
+});
+
+document.querySelector(".again").addEventListener("click", function () {
+  score = 20;
+  SecretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector(".message").textContent = "Start Guessing!";
+  document.querySelector(".score").textContent = score;
+  document.querySelector(".number").textContent = "?";
+  document.querySelector(".guess").textContent = "";
+  document.querySelector("body").style.backgroundColor = "#222";
+  document.querySelector(".number").style.width = "15rem";
 });
